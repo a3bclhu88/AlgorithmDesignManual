f17e0cf4fd125a51478b1cf9d9422934b9664876
Andy <Andy@192.168.0.13>
2017-09-27 06:29:17 -0400

    vertix square using adjacent list

--- src/function/GrapghList.java
+++ src/function/GrapghList.java
@@ -7,15 +7,18 @@ public class GrapghList {
 	int[][] matrix = {{0,0,1,0,0,1},{0,0,0,0,1,0},{1,0,0,0,0,0},{0,0,0,0,0,1},{0,1,0,0,0,1},{1,0,0,1,1,0}};
 	LinkedList<LinkedList<Integer>> adjacentList;
 	int[][] matrix2;
+	int[][] matrix3;
 	LinkedList<LinkedList<Integer>> adjacentList2;
 	ArrayList<ArrayList<Integer>> middleCounter;
-	
+	//ArrayList<ArrayList<Integer>> middleCounter2;
 	public GrapghList(){
 		
 		matrix2 = new int[6][6];
+		matrix3 = new int[6][6];
 		for(int i = 0; i< 6; i++){
 			for(int j = 0; j< 6; j++){
 				matrix2[i][j] = 0;
+				matrix3[i][j] = 0;
 			}
 		}
 		middleCounter = new ArrayList<ArrayList<Integer>>();
@@ -48,6 +51,21 @@ public class GrapghList {
 		display(matrix2);
 		
 		
+		
+		adjacentList = new LinkedList<LinkedList<Integer>>();
+		matrixToList(matrix,adjacentList);
+		//middleCounter2= new ArrayList<ArrayList<Integer>>();
+		for(int i = 0;i<adjacentList.size();i++){
+			LinkedList<Integer> L1 = adjacentList.get(i);
+			for(int v: L1){
+				for(int u:L1){
+					if(u!=v){
+						matrix3[v][u] = 1;
+					}
+				}
+			}
+		}
+		display(matrix3);
 	}
 	
 	void display(int[][] m){
@@ -58,5 +76,17 @@ public class GrapghList {
 			}
 			System.out.println("");
 		}
+	}
+	void matrixToList(int[][] m, LinkedList<LinkedList<Integer>> l){
+		for(int i = 0; i< 6;i++){
+			l.add(new LinkedList<Integer>());
+			for(int j = 0; j< 6;j++){
+				if(m[i][j] == 1){
+					l.get(i).add(j);
+				}
+			}
+			System.out.println("size:" + l.get(i).size());
+		}
+		System.out.println("size:" + l.size());
 	}
 }
